3fb4528479e9c002fa920f0f17dbdfe760eeaa2c
Blaise <blaisetiong1030@gmail.com>
2020-10-07 10:30:25 +0800

    Game name

--- pong.ts
+++ pong.ts
@@ -32,14 +32,16 @@ function initialGameState(): GameState {
       paddle2State: { svgType: 'rect', x: 682, y: 215, width: 8, height: 70, fill: 'white' },
       scoreBoardState: { svgType: 'text', x: 285, y: 80, fill: 'white', 'font-size': '70px' },
       paddleSizeIncreaseState: { svgType: 'rect', x: 0, y: 0, fill: 'red', width: 0, height: 0 },
-      slowMyBallState: { svgType: 'rect', x: 0, y: 0, fill: 'blue', width: 0, height: 0 }
+      slowMyBallState: { svgType: 'rect', x: 0, y: 0, fill: 'blue', width: 0, height: 0 },
     },
   }
 }
 function speedProgression(n: number): number {
+  // This returns the distance where the computer paddle will start following the ball 
   return (n <= 3) ? 400 : (n > 3 && n <= 6) ? 200 : (n > 6) ? 0 : null;
 }
 function onYCollision1(bs: BallState): BallState {
+  // Returns a new ball state when the ball collides with the ceiling or floor
   return {
     ...bs,
     cx: bs.cx - bs.vx,
@@ -48,8 +50,7 @@ function onYCollision1(bs: BallState): BallState {
   }
 }
 function onTravelX1(acc: BallState, paddle: PaddleState): BallState {
-  /* This function returns a new ball state depends on which part the paddle is hit.
-  If it is not hit, the ball will keep traveling in a straight line */
+  // In charge of ball collisions on paddles as well as keeping the ball running to the next position
   if (acc.cx + acc.r <= paddle.x + paddle.width && acc.cx + acc.r >= paddle.x
     || acc.cx - acc.r <= paddle.x + paddle.width && acc.cx - acc.r >= paddle.x) {
     if (acc.cy > paddle.y && acc.cy <= paddle.y + paddle.height / 4)
@@ -65,7 +66,8 @@ function onTravelX1(acc: BallState, paddle: PaddleState): BallState {
   }
   return { ...acc, cx: acc.cx - acc.vx, cy: acc.cy - acc.vy }
 }
-function onBallHitsPaddleUpgrade(gs: GameState) {
+function onBallHitsPaddleUpgrade(gs: GameState): GameState {
+  // Updates the game state when the ball hits the upgrade paddle red square
   return ({
     ...gs,
     uiStates: {
@@ -83,6 +85,7 @@ function onBallHitsPaddleUpgrade(gs: GameState) {
   })
 }
 function onBallHitsSlowDown(gs: GameState): GameState {
+  // Updates the game state when the ball hits the slow Ball down blue square
   return ({
     ...gs,
     uiStates: {
@@ -94,6 +97,7 @@ function onBallHitsSlowDown(gs: GameState): GameState {
   })
 }
 function updatePaddle1Pos(gs: GameState, yVal: number): GameState {
+  // Returns a new game state with a updated y value for the player's paddle
   return ({
     ...gs, uiStates:
     {
@@ -106,6 +110,7 @@ function updatePaddle1Pos(gs: GameState, yVal: number): GameState {
   })
 }
 function updatePaddle2Pos(gs: GameState, up: Boolean): GameState {
+  // Updates the computer's paddle eg. either going up by one px or down
   return {
     ...gs, uiStates: {
       ...gs.uiStates, paddle2State: {
@@ -116,6 +121,8 @@ function updatePaddle2Pos(gs: GameState, up: Boolean): GameState {
   }
 }
 function computerAI(gs: GameState): GameState {
+  // This function will return a new game state depends of the position of the ball on the canvas 
+  // as well as the distance where the paddle will start following the ball
   if (uiStateAccess(gs, 'ballState', 'cy') <
     uiStateAccess(gs, 'paddle2State', 'y') + uiStateAccess(gs, 'paddle2State', 'height') / 2
     && uiStateAccess(gs, 'paddle2State', 'y') > 5
@@ -130,6 +137,7 @@ function computerAI(gs: GameState): GameState {
     return gs
 }
 function paddleUpgradeSpots(gs: GameState, newX: number, newY: number): GameState {
+  // This functions returns a new game state with the new position of the red square
   return {
     ...gs, uiStates: {
       ...gs.uiStates,
@@ -145,6 +153,7 @@ function paddleUpgradeSpots(gs: GameState, newX: number, newY: number): GameStat
   }
 }
 function slowBallSpots(gs: GameState, newX: number, newY: number): GameState {
+  // Returns a new game state with a updated blue square
   return {
     ...gs, uiStates: {
       ...gs.uiStates,
@@ -160,7 +169,7 @@ function slowBallSpots(gs: GameState, newX: number, newY: number): GameState {
   }
 }
 function reduceGameState(acc: GameState, changes: Paddle1YChanges | Paddle2YChanges | RunBall | PaddleUpgrade | SlowMyBall): GameState {
-  /* This functaion returns a new game state with changes applied */
+  // This takes in changes and merge them into a new game state 
   if (changes instanceof Paddle1YChanges)
     return updatePaddle1Pos(acc, changes.newY);
   if (changes instanceof Paddle2YChanges)
@@ -181,9 +190,7 @@ function renderEngine(gs: GameState, canvas: String): Array<Element> {
         .reduce(
           (acc: Element, [key, value], index: number) => {
             acc.setAttribute(key, String(value));
-            if (index === 3) {
-              acc.innerHTML = String(gs.playerScore + ':' + gs.computerScore)
-            }
+            acc.innerHTML = String(gs.playerScore + ':' + gs.computerScore)
             return acc
           }, document.createElementNS(document.getElementById(String(canvas))
             .namespaceURI, String(UIAttr.svgType))
@@ -192,26 +199,36 @@ function renderEngine(gs: GameState, canvas: String): Array<Element> {
   )
 }
 function uiStateAccess(gs: GameState, element: string, attr: string): number {
+  // This function just allows faster access to attributes of the ui elements 
   return gs.uiStates[element][attr]
 };
 function ballEngine(): Observable<RunBall> {
+  // This function returns a stream of RunBall in every interval 
   return interval(1).pipe(map((_) => new RunBall()))
 }
 function paddle2Engine(): Observable<Paddle2YChanges> {
+  // Similar to ballEngine this function returns a stream of Paddle2YChanges
   return interval(1).pipe(map((_) => new Paddle2YChanges()))
 }
 function spawnPaddleUpgrade(): Observable<PaddleUpgrade> {
+  // This stream is in charge to produce a stream of PaddleUpgarde which will have random coordinates
+  // on the canvas in every 30 seconds, this helps us spawn red squares over the canvas 
   return interval(30000).pipe(map((_) => new PaddleUpgrade(300 + Math.random() * 100, Math.random() * 500)))
 }
 function spawnSlowMyBall(): Observable<SlowMyBall> {
+  // This stream is in charge to produce a stream of SlowMyBall which will have random coordinates
+  // on the canvas in every 32 seconds, this helps us spawn blue square over the canvas
   return interval(32000).pipe(map((_) => new SlowMyBall(300 + Math.random() * 100, Math.random() * 500)))
 }
 function playerMouse(): Observable<Paddle1YChanges> {
+  // This function produces a stream of PaddleIYChanges based on the mouse event on the canvas
   return fromEvent<MouseEvent>(document.getElementById('canvas'), 'mousemove')
     .pipe(map(({ clientY }) => new Paddle1YChanges(clientY - 105)))
 }
 function updateBallPos(gs: GameState): GameState {
-  /* Updates the ball positions for the function reduceGameState */
+  // This function will detect the conditions of the ball and returns a new game state based on those conditions
+  // For example, if the ball has collided with paddles, collided with the ceiling or floor, collided with power ups
+  // or if someone has won the match
   if (uiStateAccess(gs, 'ballState', 'cx') > uiStateAccess(gs, 'paddleSizeIncreaseState', 'x') &&
     uiStateAccess(gs, 'ballState', 'cx') < uiStateAccess(gs, 'paddleSizeIncreaseState', 'x')
     + uiStateAccess(gs, 'paddleSizeIncreaseState', 'width') &&
@@ -219,7 +236,7 @@ function updateBallPos(gs: GameState): GameState {
     uiStateAccess(gs, 'ballState', 'cy') < uiStateAccess(gs, 'paddleSizeIncreaseState', 'y')
     + uiStateAccess(gs, 'paddleSizeIncreaseState', 'height')
     && uiStateAccess(gs, 'ballState', 'vx') < 0)
-    return onBallHitsPaddleUpgrade(gs);
+    return onBallHitsPaddleUpgrade(gs); 
   else if ((uiStateAccess(gs, 'ballState', 'cx') > uiStateAccess(gs, 'slowMyBallState', 'x') &&
     uiStateAccess(gs, 'ballState', 'cx') < uiStateAccess(gs, 'slowMyBallState', 'x')
     + uiStateAccess(gs, 'slowMyBallState', 'width') &&
@@ -231,6 +248,7 @@ function updateBallPos(gs: GameState): GameState {
     return { ...gs, uiStates: { ...gs.uiStates, ballState: onYCollision1(gs.uiStates.ballState) } };
   else if (gs.uiStates.ballState.cx < 0 || gs.uiStates.ballState.cx > 700)
     return someoneScores(gs)
+  // If nothing happens
   return {
     ...gs, uiStates: {
       ...gs.uiStates, ballState: onTravelX1(gs.uiStates.ballState, gs.uiStates.ballState.vx > 0 ?
@@ -240,10 +258,15 @@ function updateBallPos(gs: GameState): GameState {
   };
 }
 function someoneScores(gs: GameState): GameState {
-  if (gs.uiStates.ballState.cx < 0 && gs.computerScore + 1 === 9)
+  // This function detects who scores and updates the corresponding game state
+  if (gs.uiStates.ballState.cx < 0 && gs.computerScore + 1 === 7) {
+    alert('Oops Loser !')
     return initialGameState()
-  else if (gs.uiStates.ballState.cx > 0 && gs.playerScore + 1 === 9)
+  }
+  else if (gs.uiStates.ballState.cx > 0 && gs.playerScore + 1 === 7) {
+    alert('You won !')
     return initialGameState()
+  }
   return gs.uiStates.ballState.cx < 0 ? {
     uiStates: {
       ...initialGameState().uiStates,
@@ -269,6 +292,7 @@ function someoneScores(gs: GameState): GameState {
     }
 }
 function pong() {
+  // Our game merges different streams and feeds it to scan the updates the changes.
   merge(
     playerMouse(),
     paddle2Engine(),
